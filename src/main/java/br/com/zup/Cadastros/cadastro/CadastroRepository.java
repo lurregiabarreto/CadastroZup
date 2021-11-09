eebe0a591a553e821ec81a4ed070ae4c8f974f3e
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-09 20:30:45 -0300

    adicionar lista para percorrer

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -2,5 +2,12 @@ package br.com.zup.Cadastros.cadastro;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CadastroRepository extends CrudRepository<Cadastro, String> {
+    List<Cadastro> findAllByMoraSozinho(boolean moraSozinho);
+
+    List<Cadastro> findAllByIdade(int idade);
+
+    List<Cadastro> findAllByTemPet(boolean temPet);
 }
