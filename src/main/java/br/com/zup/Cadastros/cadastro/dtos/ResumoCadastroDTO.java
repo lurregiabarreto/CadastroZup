06196155e08cd943f70ae3f36474f4b39c794468
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-09 20:20:29 -0300

    adicionar métodos get e set a classe resumo cadastro dto

--- src/main/java/br/com/zup/Cadastros/cadastro/dtos/ResumoCadastroDTO.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/dtos/ResumoCadastroDTO.java
@@ -1,2 +1,35 @@
-package br.com.zup.Cadastros.cadastro.dtos;public class ResumoCadastroDTO {
+package br.com.zup.Cadastros.cadastro.dtos;
+
+public class ResumoCadastroDTO {
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+
+    public ResumoCadastroDTO() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 }
+
